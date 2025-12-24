30857742b920f22373271a2aff6c0892788d9b57
Chan Shu You <you.sebastian.1.618@Chans-MacBook-Pro.local>
2025-12-24 02:45:41 -0800

    changed layout and css

--- src/components/MainInput/MainInput.tsx
+++ src/components/MainInput/MainInput.tsx
@@ -1,4 +1,6 @@
+'use client'
 import './style.scss';
+import {useState} from "react";
 
 type Props = {
   question?: string;
@@ -6,6 +8,9 @@ type Props = {
 }
 
 export default function MainInput({ question, sampleInputs }: Props) {
+
+  const [mainInput, setMainInput] = useState('');
+
   return (
     <div className={'main-input-container'}>
       {question && (
@@ -13,16 +18,22 @@ export default function MainInput({ question, sampleInputs }: Props) {
       )}
       <input type={'text'}
              className={'main-input'}
+             value={mainInput}
+             onChange={(e) => setMainInput(e.target.value)}
+
        />
       {sampleInputs && (
-        <div className={'sample-inputs'}>
-          <label className={'sample-input-label'}>Sample Inputs: </label>
-          {sampleInputs.map((item) => {
-            return (
-              <span key={item} className={'sampleInput'}>{item}</span>
-            )
-          })}
-        </div>
+        <>
+          <i className={'sample-input-label'}>Sample Inputs: &nbsp;</i>
+          <div className={'sample-inputs'}>
+            {sampleInputs.map((item) => {
+              return (
+                <span key={item} className={'sample-input'} onClick={() => setMainInput(item)}>{item}</span>
+              )
+            })}
+          </div>
+        </>
+
       )}
     </div>
   )
